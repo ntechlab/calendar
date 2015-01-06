33c4bd11dbe9ab7886ff3a4d9cce40491af09ce3
tnog2014 <tnog@example.com>
2015-01-06 22:34:18 +0900

    複数ファイル対応

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,47 @@ var express = require('express');
 var router = express.Router();
 var um_utils = require('../lib/um_utils');
 
-/* GET home page. */
+var path = './public/json/';
+
 var fs = require('fs');
-var events = fs.readFileSync('./public/json/events.json', 'utf8');
-console.log(events);
+
+function getEvents(){
+	var list = fs.readdirSync(path);
+	var events = [];
+	var classList = [];
+	list.forEach(function (v){
+		console.log(v);
+		var className = v.replace(/\.json$/, "");
+		classList.push(className);
+		var tmp = fs.readFileSync(path+"/"+v, 'utf8');
+
+		// JSONコメントを削除
+		tmp = tmp.replace(/\/\*[\s\S]*?\*\//g, '');
+
+		var obj = JSON.parse(tmp);
+		obj.forEach(function (ev){
+			ev["className"] = className;
+			events.push(ev);
+		});
+	});
+	return { events: events, classList: classList};
+}
+
 router.get('/', function(req, res) {
   var map = um_utils.getNavbarInfo(req, res);
-  map.title = 'Calendar';
-  map.data = events;
-  res.render('index', map);
+
+  // 認証されていない場合には、ログイン画面に遷移する。
+  if(req.isAuthenticated()){
+	  map.title = 'Calendar';
+	  var eventObj = getEvents();
+	  var evv = JSON.stringify(getEvents().events);
+
+	  map.data = evv;
+	  map.eventClassList = eventObj.classList;
+	  return res.render('index', map);
+  } else {
+	  return res.redirect('/auth/login');
+  }
 });
 
 module.exports = router;
