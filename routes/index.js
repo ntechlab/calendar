691c19e7e8d787da71f8d28eb8c30b92ddf6a509
tnog2014 <tnog@example.com>
2015-01-07 14:01:34 +0900

    node版underscore.jsを利用するように修正。祝日を表示できるように修正。

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,43 @@
 var express = require('express');
 var router = express.Router();
 var um_utils = require('../lib/um_utils');
+var u = require('underscore');
+var fs = require('fs');
 
 var path = './public/json/';
-
-var fs = require('fs');
 
 function getEvents(){
 	var list = fs.readdirSync(path);
 	var events = [];
 	var classList = [];
+	var holidays= convertFileToEventObject(path + "/static/holiday.json", 'utf8');
+	u.each(holidays, function (ev){
+		ev.className = "holiday";
+		ev.editable = false;
+		events.push(ev);
+	});
 	list.forEach(function (v){
 		console.log(v);
-		var className = v.replace(/\.json$/, "");
-		classList.push(className);
-		var tmp = fs.readFileSync(path+"/"+v, 'utf8');
-
-		// JSONコメントを削除
-		tmp = tmp.replace(/\/\*[\s\S]*?\*\//g, '');
-
-		var obj = JSON.parse(tmp);
-		obj.forEach(function (ev){
-			ev["className"] = className;
-			events.push(ev);
-		});
+		if(v.match("\.json$")){
+			var className = v.replace(/\.json$/, "");
+			classList.push(className);
+			var obj = convertFileToEventObject(path+"/"+v, 'utf8');
+			u.each(obj, function (ev){
+				ev["className"] = className;
+				events.push(ev);
+			});
+		}
 	});
+	
+	console.dir(events);
 	return { events: events, classList: classList};
+}
+
+function convertFileToEventObject(path, encoding){
+	var contents = fs.readFileSync(path, encoding);
+	contents = contents.replace(/\/\*[\s\S]*?\*\//g, '');
+	var obj = JSON.parse(contents);
+	return obj;
 }
 
 router.get('/', function(req, res) {
