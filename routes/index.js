a0ac760a88ce79e7aa44586c453f2e3aa6a88541
babibon <babibon@example.com>
2015-03-16 22:30:21 +0900

    イベント追加機能作成

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var um_utils = require('../lib/um_utils');
 var u = require('underscore');
 var fs = require('fs');
+var moment = require('../public/javascripts/lib/moment.min');
 
 var path = './public/json/';
 
@@ -39,6 +40,7 @@ function processJSON(filePath) {
 	var obj = convertFileToEventObject(filePath, 'utf8');
 	var fileInfo = obj.fileInfo;
 	var globalProperty = fileInfo.globalProperty;
+	var fileName = filePath.substring(filePath.lastIndexOf('/')+1, filePath.length);
 	u.each(obj.event, function (ev){
 		// 各イベントにグローバルプロパティを設定
 		u.each(globalProperty, function (value, key){
@@ -53,7 +55,7 @@ function processJSON(filePath) {
 			return eg.groupId == ev.groupId;
 		});
 		if(isFirstGroup){
-			var eventGroup = { groupId: ev.groupId, groupName: ev.groupName, className: ev.className};
+			var eventGroup = { groupId: ev.groupId, groupName: ev.groupName, className: ev.className, fileName: fileName};
 			eventGroupList.push(eventGroup);
 		};
 	});
@@ -147,29 +149,6 @@ function convertFileToEventObject(path, encoding){
 	return obj;
 }
 
-function writeJsonFile(filePath, contents, encoding){
-//	var contents = fs.readFileSync(filePath, encoding);
-	contents = contents.replace(/\/\*[\s\S]*?\*\//g, '');
-//	fs.writeFile(path + "/testA.json", contents, encoding, function(err) {
-//		console.log("ファイル書き込みしました");
-//		console.log("err" + err);
-//	});
-	var obj = JSON.parse(contents);
-	fs.writeFile(filePath, JSON.stringify(obj, '', '    '), function(err) {
-		console.log("またまたファイル書き込みしました");
-		console.log("err" + err);
-	});
-	try {
-		// 空ファイル作成（すでに存在していたらエラー）
-		fs.writeFileSync(filePath, "", {flag: "wx"});
-	} catch (err) {
-		console.log("ファイル[" + filePath + "]はすでに存在します。");
-		return false;
-	}
-	
-	return obj;
-}
-
 router.get('/', function(req, res) {
   var map = um_utils.getNavbarInfo(req, res);
 
@@ -189,32 +168,89 @@ router.get('/', function(req, res) {
 
 module.exports = router;
 
-//JSONファイル作成処理
+/**
+ * イベント追加
+ * カレンダーよりイベント追加リクエストを受けて、JSONファイルにイベントを追加する。
+ * 処理後、カレンダーを表示する。
+ * アクション名：「/addEvent」
+ */
 router.post('/addEvent', function(req, res) {
-	// TODO k作成中
-//	var edittingFile = path + "editting.pid";
-//	var editOk = createEmptyFile(edittingFile);
-//	// 誰かがファイル編集中の場合
-//	if (!editOk) {
-//		// TODO イベントを作成できなかったときに画面に表示するメッセージ
-//		res.message("別ユーザがイベント編集中のためイベント[" + req.body.item1 + "]を追加できませんでした。再度操作してください。", "alert-success");
-//	}
-//	// TODO 画面からのリクエストトからファイル名を取得
-//	var targetEventFile = path + req.body.item1;
-//	var contents = fs.readFileSync(path + "/events.json", "UTF-8");
-//	var result = writeJsonFile(path + "/testA.jsona", contents, "UTF-8");
-//	if (result) {
-//		setTimeout(function(){
-//			console.log("イベント " + req.body.item1 + " を作成しました。");
-//			res.message("イベント " + req.body.item1 + " を作成しました。", "alert-success");
-//		}, 5000);
-////		res.redirect('/');
-//	} else {
-//		console.log("イベント " + req.body.item1 + " を作成できませんでした。");
-//		res.message("イベント " + req.body.item1 + " を作成しませんでした。", "alert-success");
-////		res.redirect('/');
-//	}
+
+	console.log("リクエストデータ");
+	console.log(req.body);
+	// TODO リクエストデータをJSONに変換する
+//	var data = JSON.parse(req.body.data);
+//	console.log("リクエストデータ");
+//	console.log(data);
+
+	// JSON編集中ステータスファイル
+	var edittingFile = path + "editting.pid";
+	// JSONファイル編集中にする
+	var processOK = createEmptyFile(edittingFile);
 	
+	// 誰かがJSONファイル編集中の場合は以降の処理を行わずに終了する
+	if (!processOK) {
+		res.message("別ユーザがイベント編集中のためイベント[" + addEventName + "]を追加できませんでした。", "alert-success");
+		return renderCalendar(req, res);
+	}
+	
+	try {
+		// 画面のリクエストデータから追加するイベント情報を取得
+		// TODO 項目名は暫定（タイトルとファイル名を取得する）
+		var addEventName = req.body.title;
+		var targetEventFile = req.body.group;
+
+		// 変更するJSONファイルをバックアップする
+		var timestamp = moment().format("YYYYMMDDHHmmssSSS");
+		processOK = copyFile(path + targetEventFile, path + "bk/" + targetEventFile + "." + timestamp, "UTF-8");
+		// JSONファイルをバックアップに失敗した場合は終了する
+		if (!processOK) {
+			res.message("ファイル操作に失敗。イベント[" + addEventName + "]を追加できませんでした。", "alert-success");
+			return renderCalendar(req, res);
+		}
+		
+		// JSONファイルからイベント情報を取得
+		var contents = convertFileToEventObject(path + targetEventFile, 'utf8');
+		// イベントIDの採番して新規イベントを作成
+		var addEventObject = {
+				id: "AE_" + contents.fileInfo.globalProperty.groupId + "_" + timestamp
+		};
+		// リクエスト情報から連携されたデータをマージする
+		// TODO リクエストデータをすべてマージしているが、イベント情報のみをマージするようにする
+		u.extend(addEventObject, req.body);
+		// TODO イベント情報のみをマージするようにしたら以下のdeleteは削除する
+		delete addEventObject.group;
+		
+		console.log("追加するイベント情報");
+		console.log(addEventObject);
+
+		// イベント情報にeventを追加する
+		contents.event.push(addEventObject);
+		
+		// JSONファイル更新
+		processOK = writeFile(path + targetEventFile, JSON.stringify(contents, '', '    '), "UTF-8");
+		if (processOK) {
+			console.log("イベント[" + addEventName + "]を追加しました。");
+			res.message("イベント[" + addEventName + "]を追加しました。", "alert-success");
+		} else {
+			console.log("ファイル操作に失敗。イベント[" + addEventName + "]を追加できませんでした。");
+			res.message("ファイル操作に失敗。イベント[" + addEventName + "]を追加しませんでした。", "alert-success");
+		}
+	} finally {
+		deleteFile(edittingFile);
+	}
+	return renderCalendar(req, res);
+
+});
+
+/**
+ * カレンダー画面を表示する
+ * 
+ * @param req リクエストオブジェクト
+ * @param res レスポンスオブジェクト
+ * @returns カレンダー画面の表示
+ */
+function renderCalendar(req, res){
 	var map = um_utils.getNavbarInfo(req, res);
 	map.title = 'Calendar';
 	var eventObj = getEvents();
@@ -224,7 +260,7 @@ router.post('/addEvent', function(req, res) {
 	map.eventGroupList = eventObj.eventGroupList;
 	return res.render('index', map);
 
-});
+}
 
 /**
  * 空ファイルを作成する
@@ -234,6 +270,7 @@ router.post('/addEvent', function(req, res) {
  *                    ファイルを新規作成したら true
  */
 function createEmptyFile(filePath){
+	console.log("ファイル[" + filePath + "]を作成します。");
 	try {
 		// 空ファイル作成（すでに存在していたらエラー）
 		fs.writeFileSync(filePath, "", {flag: "wx"});
@@ -242,6 +279,71 @@ function createEmptyFile(filePath){
 		return false;
 	}
 	console.log("ファイル[" + filePath + "]を作成しました。");
+	return true;
+}
+
+/**
+ * ファイルをコピーする作成する
+ * 
+ * @param orgFilePath コピー元ファイルパス
+ * @param copyFilePath コピー先ファイルパス
+ * @param encoding エンコード
+ * @returns {Boolean} ファイルがすでに存在していたら false<br>
+ *                    ファイルを新規作成したら true
+ */
+function copyFile(orgFilePath, copyFilePath, encoding){
+	console.log("ファイル[" + orgFilePath + "]を[" + copyFilePath + "]にコピーします。");
+	try {
+		var contents = fs.readFileSync(orgFilePath, encoding);
+		// ファイルをコピーして書き込み（すでに存在していたらエラー）
+		fs.writeFileSync(copyFilePath, contents, {flag: "wx"});
+	} catch (err) {
+		console.log("ファイル[" + copyFilePath + "]はすでに存在します。");
+		return false;
+	}
+	console.log("ファイル[" + orgFilePath + "]を[" + copyFilePath + "]にコピーしました。");
+	return true;
+}
+
+/**
+ * ファイルを削除する
+ * 
+ * @param filePath ファイルパス
+ * @returns {Boolean} ファイルが削除できなかったら false<br>
+ *                    ファイルを削除したら true
+ */
+function deleteFile(filePath){
+	console.log("ファイル[" + filePath + "]を削除します。");
+	try {
+		// ファイル削除
+		fs.unlinkSync(filePath);
+	} catch (err) {
+		console.log("ファイル[" + filePath + "]が削除せきませんでした。");
+		return false;
+	}
+	console.log("ファイル[" + filePath + "]を削除しました。");
+	return true;
+}
+
+/**
+ * ファイルに書き込む
+ * 
+ * @param filePath ファイルパス
+ * @param contents 書き込む内容
+ * @param encoding エンコード
+ * @returns {Boolean} ファイルに書き込めなかったら false<br>
+ *                    ファイルに書き込みしたら true
+ */
+function writeFile(filePath, contents, encoding){
+	console.log("ファイル[" + filePath + "]に書き込みします。");
+	try {
+		fs.writeFileSync(filePath, contents, {encoding: encoding});
+	} catch (err) {
+		console.log("ファイル[" + filePath + "]に書き込めませんでした。");
+		console.log("書き込み内容[" + contents + "]");
+		return false;
+	}
+	console.log("ファイル[" + filePath + "]に書き込みしました。");
 	return true;
 }
 
